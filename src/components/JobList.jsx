d525ea1226cde7121bfc35399aec2ccaa3cdc490
Mayank <agarwalm92@gmail.com>
2024-04-28 00:03:56 +0530

    Added react-icons

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 /* eslint-disable react/prop-types */
 const JobList = ({ j }) => {
@@ -32,7 +33,7 @@ const JobList = ({ j }) => {
         <div className="border border-gray-100 mb-5"></div>
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline mb-1 mr-1 text-lg" />
             {j.location}
           </div>
           <a
