7278a458fdffae6f4d7770455bab65fe13561e7b
Mayank <agarwalm92@gmail.com>
2024-04-27 20:43:40 +0530

    Adding components, logo, favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import BrowseJobs from "./components/BrowseJobs";
+import Developers from "./components/Developers";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <p className="text-5xl">Job posting</p>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Developers />
+      <BrowseJobs />
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
 }
 
 export default App;
