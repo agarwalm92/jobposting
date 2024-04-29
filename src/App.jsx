bcc8cb612031b2faaa9f3dff8847e530c85abe56
Mayank <agarwalm92@gmail.com>
2024-04-30 00:12:31 +0530

    Adding react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
+/* eslint-disable no-unused-vars */
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
 import BrowseJobs from "./components/BrowseJobs";
 import HomeCards from "./components/HomeCards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import ViewAllJobs from "./components/ViewAllJobs";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>MYApp</h1>} />)
+);
 function App() {
   return (
     <>
-      <Navbar />
+      <RouterProvider router={router} />
+      {/* <Navbar />
       <Hero />
       <HomeCards />
       <BrowseJobs />
-      <ViewAllJobs />
+      <ViewAllJobs /> */}
     </>
   );
 }
